9827ed143aed9f344072d527b32ccd300a2e37b9
matuzalema <matuzalema@gmail.com>
2019-05-02 16:53:43 +0200

    Initialize project

---
+++ src/Comment.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Comment = ({text, votes, id, thumbUpComment}) =>
+  <li>
+    {text} <span>votes: {votes}</span> <button onClick={() => thumbUpComment(id)}>Thumb up</button>
+  </li>;
+
+export default Comment;
+
